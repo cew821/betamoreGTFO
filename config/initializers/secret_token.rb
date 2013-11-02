1428836d4db40e3dbde85e3ef686088733d7156b
Charles Worthignton <c.e.worthington@gmail.com>
2013-11-02 15:20:19 -0400

    made it into a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BetamoreGTFO::Application.config.secret_key_base = '6ae328b26ebff6a22b105588d244375f26302ab9ed16e7d339c0ea653abfd1d206ffb4b361494ffd0f85aa1edfc955d3b0ac287fc4197ea7dd3f1327902b4304'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BetamoreGTFO::Application.config.session_store :cookie_store, key: '_betamoreGTFO_session'
